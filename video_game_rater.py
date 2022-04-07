d88aa7a2b2f7a18b9e0431cf2274abab71788114
gabrielle-esguerra <103225491+gabrielle-esguerra@users.noreply.github.com>
2022-04-08 11:44:39 +1200

    Main routine , Initial Collection
    Added some games with some initial ratings

---
+++ video_game_rater.py
@@ -0,0 +1,11 @@
+##
+# video_game_rater.py
+# Rates video games
+
+if __name__ == "__main__":
+    videogames = {"Minecraft":5,
+                  "Call of Duty":1,
+                  "Angry Birds":4,
+                  "Splatoon 2":5,
+                  "Animal Crossing":4}
+    
