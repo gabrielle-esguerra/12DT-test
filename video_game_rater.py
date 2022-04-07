147c4c8f912f56674be0c7314db2b7a8a53b32b5
gabrielle-esguerra <103225491+gabrielle-esguerra@users.noreply.github.com>
2022-04-08 11:48:45 +1200

    Added print video games function

--- video_game_rater.py
+++ video_game_rater.py
@@ -2,10 +2,19 @@
 # video_game_rater.py
 # Rates video games
 
+def print_dictionary(dictionary):
+    """
+    Accepts a dictionary , loops through it and
+    prints out the game and its rating
+    """
+    for game, rating in dictionary.items():
+        print("Game: {}\tRating: {}".format (game, rating))
+
 if __name__ == "__main__":
     videogames = {"Minecraft":5,
                   "Call of Duty":1,
                   "Angry Birds":4,
                   "Splatoon 2":5,
                   "Animal Crossing":4}
+    print_dictionary(video_games)
     
