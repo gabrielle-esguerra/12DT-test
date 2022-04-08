08d278dfdf0813646638e21af51ed0e547d3f525
gabrielle-esguerra <103225491+gabrielle-esguerra@users.noreply.github.com>
2022-04-08 12:18:22 +1200

    Converted to Multidimensional

--- video_game_rater.py
+++ video_game_rater.py
@@ -7,14 +7,19 @@ def print_dictionary(dictionary):
     Accepts a dictionary , loops through it and
     prints out the game and its rating
     """
-    for game, rating in dictionary.items():
-        print("Game: {}\tRating: {}".format (game, rating))
+    # outer dictionary
+    for id, game in dictionary.items():
+        print("Id: {} Game: {}\tRating: {}".format (id,
+                                                    game["name"],
+                                                    game["rating"]))
 
 if __name__ == "__main__":
-    videogames = {"Minecraft":5,
-                  "Call of Duty":1,
-                  "Angry Birds":4,
-                  "Splatoon 2":5,
-                  "Animal Crossing":4}
+    
+    videogames = {1:{"name":"Minecraft", "rating":5}
+                  2:{"name":"Call of Duty","rating":1}
+                  3:{"name":"Angry Birds", "rating":4}
+                  4:{"name":"Splatoon 2", "rating":5}
+                  5:{"name":"Animal Crossing","rating":4}}
+    
     print_dictionary(video_games)
     
